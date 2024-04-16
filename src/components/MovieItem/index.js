b9ac7351dd5ec5ba31e079f3c7c179a3a19878a6
ramya <ramyapotikalapudi@gmail.com>
2024-04-16 08:32:57 +0000

    solution

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,41 @@
+// Write your code here
+import ReactPlayer from 'react-player'
+import Popup from 'reactjs-popup'
+
+import {IoMdClose} from 'react-icons/io'
+
+import 'reactjs-popup/dist/index.css'
+
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={
+          <img src={thumbnailUrl} alt="thumbnail" className="thumbnail" />
+        }
+        className="popup-content"
+      >
+        {close => (
+          <div>
+            <button
+              type="button"
+              className="trigger-button"
+              data-testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose />
+            </button>
+            <ReactPlayer url={videoUrl} controls />
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+export default MovieItem
